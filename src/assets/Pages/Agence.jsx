cfd09dd8c81ddf7cbb8e5c26165b188180597163
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-08-28 09:17:07 +0530

    setting up

--- src/assets/Pages/Agence.jsx
+++ src/assets/Pages/Agence.jsx
@@ -1,22 +1,50 @@
 import React, { useRef } from 'react'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
 
 const Agence = () => {
 
+  gsap.registerPlugin(ScrollTrigger)
+
   const imageDivRef = useRef(null)
+  const imageRef = useRef(null)
+
+  const imageArray = [
+    "https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7 ", 
+    "https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64 ",
+    "https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991",
+    "https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2",
+    "https://k72.ca/images/teamMembers/MEL_480X640.jpg?w=480&h=640&fit=crop&s=07c9bfee89816720b873e6748a276af6",
+    "https://k72.ca/images/teamMembers/CAMILLE_480X640_2.jpg?w=480&h=640&fit=crop&s=74317575b2d72fd11c5296615c383e4a",
+    "https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847",
+    "https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6",
+    "https://k72.ca/images/teamMembers/PLP2_640x960.jpg?w=640&h=960&s=277b83adbf80a1c60a1c7ee72dfdf5cf",
+    "https://k72.ca/images/teamMembers/joel_640X960_3.jpg?w=640&h=960&s=7fcd2cb26f9ffb7cb96fda9c5e707496",
+    
+  ]
+
+  console.log(imageArray)
 
   useGSAP(function(){
-   gsap.to(imageDivRef.current,{
-    x:500,
-    repeat:5 
-   })
+  gsap.to(imageDivRef.current,{
+    scrollTrigger:{
+      trigger:imageDivRef.current,
+      markers:true,
+      start:'top 28.8%',
+      end:'top -70%',
+      pin: true  ,
+      onUpdate:(elem)=>{
+        // console.log(elem )
+      }
+    }
+  }) 
 })
   return (
 <div>
       <div className='section1'>
-      <div className=' absolute overflow-hidden  h-75 w-57 ml-110  top-60 rounded-3xl'> 
-        <img ref={imageDivRef} className='h-full object-cover w-full' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
+      <div ref={imageDivRef} className=' absolute overflow-hidden  h-75 w-57 ml-110  top-60 '> 
+        <img ref={imageRef} className='h-full object-cover w-full rounded-3xl' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
       </div>
       <div className='relative font-[font2] '>
         <div className=' mt-[55vh] '>
